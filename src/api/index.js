538b405fb1393e3b5aa7140e272b6b0a3ddf8f68
chenwenjie <mschenwenjie@163.com>
2022-08-31 15:34:41 +0800

    项目注册接口定义和调用

--- src/api/index.js
+++ src/api/index.js
@@ -3,16 +3,33 @@
 // 引入自定义axios函数
 import request from '@/utils/request'
 // 导出接口方法，为了在逻辑页面引入后调用
-export const registerAPI = () => {
+// registerAPI(this.form)
+// registerAPI({
+//   username: '',
+//   password: '',
+//   repassword: ''
+// })
+// 形参obj的值
+// obj = {username: 值, password: '', repassword: ''}
+// 左侧想要对象解构赋值（语法）
+// {username:username变量名 , password: 变量名, repassword:变量名} = {username: 值, password: '', repassword: ''}
+// 函数形参的obj就改成对象解构接收传入的数据对象
+// {username：username， password: password, repassword: repassword}
+// key是传入的对象key匹配，value是变量名(同名)
+// ES6规定，key和value变量同名的时候，可以简写（key既为key也是value变量名）
+export const registerAPI = ({ username, password, repassword }) => {
   // 原地是一个Promise对象(内部包含原生ajax请求)
   // return 这个Promise对象到逻辑页面，去那边对Promise对象提取结果
   return request({
     url: '/api/reg',
-    method: 'post',
+    method: 'POST',
+    // axios传参params,data
+    // params的对象参数名和值，axios源码会把参数和值，拼接在url?后面给后台（query查询字符串）
+    // data的对象参数名和值，axios源码会把参数和值，拼接在请求体里（body参数）
     data: {
-      username: 'lidongxu',
-      password: '111111',
-      repassword: '111111'
+      username,
+      password,
+      repassword
     }
   })
 }
