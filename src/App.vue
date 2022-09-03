6575eeffaddabf63cb88187b8354961926b8cb37
chenwenjie <mschenwenjie@163.com>
2022-09-03 14:22:44 +0800

    获取用户信息

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,11 @@
 </template>
 
 <script>
-import { registerAPI } from '@/api/index'
+
 export default {
-  async created () {
-    const res = await registerAPI()
-    console.log(res)
-  }
 
 }
+
 </script>
 
 <style lang="less" scoped>
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 // 封装的是具体的接口请求方法
 // 注意：每个方法只负责请求一个url地址
 // 引入自定义axios函数
+import store from '@/store'
 import request from '@/utils/request'
 // 导出接口方法，为了在逻辑页面引入后调用
 // registerAPI(this.form)
@@ -17,6 +18,7 @@ import request from '@/utils/request'
 // {username：username， password: password, repassword: repassword}
 // key是传入的对象key匹配，value是变量名(同名)
 // ES6规定，key和value变量同名的时候，可以简写（key既为key也是value变量名）
+// 注册函数处理方法
 export const registerAPI = ({ username, password, repassword }) => {
   // 原地是一个Promise对象(内部包含原生ajax请求)
   // return 这个Promise对象到逻辑页面，去那边对Promise对象提取结果
@@ -33,7 +35,7 @@ export const registerAPI = ({ username, password, repassword }) => {
     }
   })
 }
-
+// 登录函数处理方法
 export const loginAPI = ({ username, password }) => {
   return request({
     url: '/api/login',
@@ -41,6 +43,19 @@ export const loginAPI = ({ username, password }) => {
     data: {
       username,
       password
+    }
+  })
+}
+// 获取用户信息函数处理方法
+export const getUserInfoAPI = () => {
+  return request({
+    url: '/my/userinfo',
+    // method不写默认就是'get'方式请求
+    // 传参给后台：params(查询字符串query),data(请求体body),headers(请求头)
+    headers: {
+      // this.$store.state.token 这里this不是组件对象不能用this.$store拿到store对象
+      Authorization: store.state.token
+
     }
   })
 }
